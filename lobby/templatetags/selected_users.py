269cec20fd69b5714a4df3039d6c33832e2dc5c8
Alexander Mucalov <alexander.mucalov@gmail.com>
2014-04-04 21:33:54 -0400

    Added requirements.txt

--- lobby/templatetags/selected_users.py
+++ lobby/templatetags/selected_users.py
@@ -2,6 +2,7 @@ from django import template
 from game.gamemetamethods import get_all_logged_in_users, get_all_waitroom_users
 register = template.Library()
 
+
 @register.inclusion_tag('lobby/logged_users.html')
 def render_logged_in_user_list():
     return { 'logged_users': get_all_logged_in_users() }
