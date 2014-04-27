8ef8fd428244867c9e02caf1b1790aadc3a3cc73
Alexander Mucalov <alexander.mucalov@gmail.com>
2014-04-27 19:42:09 -0400

    Made robots.txt django-friendly

--- olympyo/urls.py
+++ olympyo/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
             # url(r'^$', 'olympyo.views.home', name='home'),
             # url(r'^blog/', include('blog.urls')),
 
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
